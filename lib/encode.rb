14ce021123e7d7637ea47bea3710ca5078a8d250
Antonio Jackson <antoniojackson@gmail.com>
2020-04-18 10:48:32 -0600

    Adding test_it_has_attributes to EncodeTest and message, key, and date attributes to the Encode class

--- lib/encode.rb
+++ lib/encode.rb
@@ -1,5 +1,9 @@
 class Encode
+  attr_reader :message, :key, :date
 
   def initialize(message, key, date)
+    @message = message.downcase
+    @key = key
+    @date = date
   end
 end
--- test/encode_test.rb
+++ test/encode_test.rb
@@ -2,8 +2,17 @@ require_relative 'test_helper'
 
 class EncodeTest < Minitest::Test
 
+  def setup
+    @encode = Encode.new("HELLO WORLD", "02715", "040895")
+  end
+
   def test_it_exists
-    encrypt = Encode.new("hello world", "02715", "040895")
-    assert_instance_of Encode, encrypt
+    assert_instance_of Encode, @encode
+  end
+
+  def test_it_has_attributes
+    assert_equal "hello world", @encode.message
+    assert_equal "02715", @encode.key
+    assert_equal "040895", @encode.date
   end
 end
