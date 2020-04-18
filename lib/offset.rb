f238e017941c96ece639e9122c50af2df16830b8
Antonio Jackson <antoniojackson@gmail.com>
2020-04-17 18:45:39 -0600

    Adding create_offsets to Offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -8,4 +8,13 @@ class Offset
   def square_date
     date.to_i * date.to_i
   end
+
+  def create_offsets
+    {
+      A: square_date.to_s[-4].to_i,
+      B: square_date.to_s[-3].to_i,
+      C: square_date.to_s[-2].to_i,
+      D: square_date.to_s[-1].to_i
+    }
+  end
 end
