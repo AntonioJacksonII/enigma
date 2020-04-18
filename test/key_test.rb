697817106537c1a4f2b13a8e85e92267c1d55681
Antonio Jackson <antoniojackson@gmail.com>
2020-04-17 19:06:18 -0600

    Adding key attribute to Key class with test in KeyTest class

--- test/key_test.rb
+++ test/key_test.rb
@@ -2,9 +2,16 @@ require_relative 'test_helper'
 
 class KeyTest < Minitest::Test
 
+  def setup
+    @key = Key.new("02715")
+  end
+
   def test_it_exists
-    key = Key.new(key)
-    assert_instance_of Key, key
+    assert_instance_of Key, @key
+  end
+
+  def test_it_has_attributes
+    assert_equal "02715", @key.key
   end
 
 end
