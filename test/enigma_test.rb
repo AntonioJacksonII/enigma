7abbf8512d27e9492bf1ee3e79450db95242c867
Antonio Jackson <antoniojackson@gmail.com>
2020-04-20 14:34:34 -0600

    Adding test_decrypt to EnigmaTest class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,4 +14,9 @@ class EnigmaTest < Minitest::Test
     result = {encryption: "keder ohulw", key: "02715", date: "040895"}
     assert_equal result, @enigma.encrypt("hello world", "02715", "040895")
   end
+
+  def test_decrypt
+    result = {decryption: "hello world", key: "02715", date: "040895"}
+    assert_equal result, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
