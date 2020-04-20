a947a2b3530d41e5ec08ca0328345addecb71fe5
Antonio Jackson <antoniojackson@gmail.com>
2020-04-20 15:13:32 -0600

    Moving default date generation from Offset class to Enigma class, updating EnigmaTest class to test for the generation of the default date, and removing tests of the default date from OffsetTest class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,11 @@
 class Enigma
 
-  def encrypt(message, key, date)
+  def encrypt(message, key, date=Date.today.strftime("%m%d%y"))
     new_message = Message.new(message, key, date)
     new_message.encrypt
   end
 
-  def decrypt(message, key, date)
+  def decrypt(message, key, date=Date.today.strftime("%m%d%y"))
     new_message = Message.new(message, key, date)
     new_message.decrypt
   end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,13 +10,29 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_default_date
+    encrypted = @enigma.encrypt("hello world", "02715")
+    encrypted.stubs(:today).returns(Date.new(2020, 04,20))
+    assert_equal "042020", encrypted[:date]
+    decrypted = @enigma.decrypt(encrypted[:encryption], "02715")
+    assert_equal "042020", decrypted[:date]
+  end
+
   def test_encrypt
     result = {encryption: "keder ohulw", key: "02715", date: "040895"}
     assert_equal result, @enigma.encrypt("hello world", "02715", "040895")
+    encrypted = @enigma.encrypt("hello world", "02715")
+    encrypted.stubs(:today).returns(Date.new(2020, 04, 20))
+    result2 = {encryption: "jib qdmctpu", key: "02715", date: "042020"}
+    assert_equal result2, encrypted
   end
 
   def test_decrypt
     result = {decryption: "hello world", key: "02715", date: "040895"}
     assert_equal result, @enigma.decrypt("keder ohulw", "02715", "040895")
+    encrypted = @enigma.encrypt("hello world", "02715")
+    decrypted = @enigma.decrypt(encrypted[:encryption], "02715")
+    result2 = {decryption: "hello world", key: "02715", date: "042020"}
+    assert_equal result2, decrypted
   end
 end
