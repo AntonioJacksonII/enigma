730ad6393a37c539047524906f2174f2c9657268
Antonio Jackson <antoniojackson@gmail.com>
2020-04-20 16:51:33 -0600

    Adding Command Line Interface functionality with the encrypt.rb and decrypt.rb runner files

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,7 +11,7 @@ encrypted_message = enigma.encrypt(incoming_message)
 
 writer = File.open(ARGV[1], "w")
 
-writer.write(encrypted_message)
+writer.write(encrypted_message[:encryption])
 writer.close
 
 p "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
