78d625da61f6c40422e353dfe73c74f2e4cdf7d3
Antonio Jackson <antoniojackson@gmail.com>
2020-04-21 08:33:28 -0600

    Moving stubs to the beginning of tests for functionality

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,11 +12,11 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_default_date
+    Date.stubs(:today).returns(Date.new(2020, 04, 20))
     encrypted = @enigma.encrypt("hello world", "02715")
-    Date.stubs(:today).returns(Date.new(2020, 04, 21))
-    assert_equal "042120", encrypted[:date]
+    assert_equal "042020", encrypted[:date]
     decrypted = @enigma.decrypt(encrypted[:encryption], "02715")
-    assert_equal "042120", decrypted[:date]
+    assert_equal "042020", decrypted[:date]
   end
 
   def test_default_key
@@ -26,11 +26,11 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt
+    Date.stubs(:today).returns(Date.new(2020, 04, 20))
     result = {encryption: "keder ohulw", key: "02715", date: "040895"}
     assert_equal result, @enigma.encrypt("hello world", "02715", "040895")
     encrypted = @enigma.encrypt("hello world", "02715")
-    Date.stubs(:today).returns(Date.new(2020, 04, 21))
-    result2 = {encryption: "nib udmcxpu", key: "02715", date: "042120"}
+    result2 = {encryption: "jib qdmctpu", key: "02715", date: "042020"}
     assert_equal result2, encrypted
   end
 
